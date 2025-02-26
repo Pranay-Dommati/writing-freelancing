6c2eaaa17bb885341a91afb031125d26b2303b25
banny <bannydommati@gmail.com>
2025-02-27 01:07:23 +0530

    removing unecessary files

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import NavigationBar from './Navbar';
-import homeImage from '../images/homepage_image.png'; // Add this import
 import '../styles/landing-page.css';
 
 const LandingPage = () => {
